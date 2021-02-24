5272497ca6fb7f298b8bda784310cadaa120909f
Luca Ruggirello <luca.ruggirello95@gmail.com>
2021-02-24 12:09:03 +0100

    ending index

---
+++ js/db/team.js
@@ -0,0 +1,44 @@
+const team = [
+  {
+    name: "Marco Valtriani",
+    role: "Photography Director",
+    bio: "34 ITA He was an expert but not in a discipline that anyone could fully appreciate. He knew how to hold the cone just right so that the soft server ice-cream fell into it at the precise angle to form a perfect cone each and every time. It had taken years to perfect and he could now do it without even putting any thought behind it. Nobody seemed to fully understand the beauty of this accomplishment except for the new worker who watched in amazement.",
+    img: "img/pexels-alleksana-4271927.jpg"
+  },
+  {
+    name: "Luca Ruggirello",
+    role: "Web Designer",
+    bio: "24 ITA Here's the thing. She doesn't have anything to prove, but she is going to anyway. That's just her character. She knows she doesn't have to, but she still will just to show you that she can. Doubt her more and she'll prove she can again. We all already know this and you will too.",
+    img: "img/pexels-alleksana-4271927.jpg"
+  },
+  {
+    name: "Simone Pennacchi",
+    role: "complex table",
+    bio: "23 ITA Sitting in the sun, away from everyone who had done him harm in the past, he quietly listened to those who roamed by. He felt at peace in the moment, hoping it would last, but knowing the reprieve would soon come to an end. He closed his eyes, the sun beating down on face and he smiled. He smiled for the first time in as long as he could remember. Sitting in the sun, away from everyone who had done him harm in the past, he quietly listened to those who roamed by. He felt at peace in the moment, hoping it would last, but knowing the reprieve would soon come to an end.",
+    img: "img/pexels-alleksana-4271927.jpg"
+  },
+  {
+    name: "Giovanni Pio",
+    role: "boot main",
+    bio: "25 ITA There was a time when he would have embraced the change that was coming. In his youth, he sought adventure and the unknown, but that had been years ago. He wished he could go back and learn to find the excitement that came with change but it was useless. That curiosity had long left him to where he had come to loathe anything that put him out of his comfort zone.",
+    img: "img/pexels-alleksana-4271927.jpg"
+  },
+  {
+    name: "Mariangiongiangela",
+    role: "silver gun",
+    bio: "26 ITA Sitting in the sun, away from everyone who had done him harm in the past, he quietly listened to those who roamed by. He felt at peace in the moment, hoping it would last, but knowing the reprieve would soon come to an end. He closed his eyes, the sun beating down on face and he smiled. He smiled for the first time in as long as he could remember. Sitting in the sun, away from everyone who had done him harm in the past, he quietly listened to those who roamed by. He felt at peace in the moment, hoping it would last, but knowing the reprieve would soon come to an end.",
+    img: "img/pexels-alleksana-4271927.jpg"
+  },
+  {
+    name: "Noemi Motta",
+    role: "rugby trustee",
+    bio: "27 ITA Sitting in the sun, away from everyone who had done him harm in the past, he quietly listened to those who roamed by. He felt at peace in the moment, hoping it would last, but knowing the reprieve would soon come to an end. He closed his eyes, the sun beating down on face and he smiled. He smiled for the first time in as long as he could remember. Sitting in the sun, away from everyone who had done him harm in the past, he quietly listened to those who roamed by. He felt at peace in the moment, hoping it would last, but knowing the reprieve would soon come to an end.",
+    img: "img/pexels-alleksana-4271927.jpg"
+  },
+  {
+    name: "Mario Rossi",
+    role: "banana consensus",
+    bio: "24 ITA Sitting in the sun, away from everyone who had done him harm in the past, he quietly listened to those who roamed by. He felt at peace in the moment, hoping it would last, but knowing the reprieve would soon come to an end. He closed his eyes, the sun beating down on face and he smiled. He smiled for the first time in as long as he could remember. Sitting in the sun, away from everyone who had done him harm in the past, he quietly listened to those who roamed by. He felt at peace in the moment, hoping it would last, but knowing the reprieve would soon come to an end.",
+    img: "img/pexels-alleksana-4271927.jpg"
+  }
+]
